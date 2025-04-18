89f22b842ae57e8ce2f1db4c06879c48ded2c9e8
ImOhashi <leonardoohashi.mkt@gmail.com>
2025-04-18 20:23:09 -0300

    first commit

---
+++ src/main/java/com/ohashi/message_manager_service/MessageManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.message_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohashi/message_manager_service/MessageManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.message_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
